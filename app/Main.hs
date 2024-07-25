285d1090b98dc1d3d2d759236cf62c3735f7e8f2
rafaelrc7 <contact@rafaelrc.com>
2024-07-25 02:12:20 -0300

    Implement compiler driver with stub compilation

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,57 @@
 module Main where
 
-import qualified MyLib (someFunc)
+import qualified Settings         as S
+
+import           Control.Monad    (unless, when)
+import           GHC.IO.Handle    (hClose, hGetLine)
+import           System.Directory (doesFileExist, removeFile)
+import           System.Exit      (ExitCode (..), die, exitSuccess, exitFailure)
+import           System.FilePath  (dropExtension, replaceExtension,
+                                   takeExtension)
+import           System.Process   (CreateProcess (..), StdStream (..),
+                                   createProcess, proc, shell, waitForProcess)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  settings <- S.parseCLIArgs
+
+  let sourceFile = S.inputFile settings
+  let targetStage = S.mode settings
+  let keepIntermediateFiles = S.keepIntermediateFiles settings
+
+  doesInputFileExist <- doesFileExist sourceFile
+  unless doesInputFileExist $ die ("Input file '" ++ sourceFile ++ "' does not exist")
+  unless (takeExtension sourceFile == ".c") $ die ("Input file '" ++ sourceFile ++ "' is not a C source file")
+
+  (_, Just hout, _, ph) <- createProcess (shell "which gcc"){ std_in = NoStream, std_out = CreatePipe, std_err = NoStream }
+  waitForProcess ph >>= \exitCode -> unless (exitCode == ExitSuccess) $ die "Missing gcc in PATH"
+
+  gccPath <- hGetLine hout
+  hClose hout
+
+  let runGCC = run gccPath
+
+  let preProcessedFile = replaceExtension sourceFile ".i"
+  runGCC [ "-E", "-P", sourceFile, "-o", preProcessedFile ]
+  when (targetStage == S.PreProcessorMode) exitSuccess
+
+  let assemblyFile = replaceExtension sourceFile ".s"
+  runGCC [ "-S", preProcessedFile, "-o", assemblyFile ]
+  unless keepIntermediateFiles $ removeFile preProcessedFile
+  when (targetStage == S.CompilerMode S.CodeEmitterMode) exitSuccess
+
+  let objectFile = replaceExtension sourceFile ".o"
+  runGCC [ "-c", assemblyFile, "-o", objectFile ]
+  unless keepIntermediateFiles $ removeFile assemblyFile
+  when (targetStage == S.AssemblerMode) exitSuccess
+
+  let executableFile = dropExtension sourceFile
+  runGCC [ objectFile, "-o", executableFile ]
+  unless keepIntermediateFiles $ removeFile objectFile
+
+run :: String -> [String] -> IO ()
+run cmd flags = do
+  (_, _, _, ph) <- createProcess (proc cmd flags){ std_in = NoStream, std_out = NoStream }
+  exitCode <- waitForProcess ph
+  unless (exitCode == ExitSuccess) exitFailure
+
---
+++ app/Settings.hs
@@ -0,0 +1,86 @@
+module Settings ( CompilerMode(..)
+                , Mode(..)
+                , Settings(..)
+                , parseCLIArgs
+) where
+
+import           Options.Applicative
+
+data CompilerMode = LexerMode
+                  | ParserMode
+                  | CodeGeneratorMode
+                  | CodeEmitterMode
+ deriving (Show, Eq)
+
+data Mode = PreProcessorMode
+          | CompilerMode CompilerMode
+          | AssemblerMode
+          | LinkerMode
+ deriving (Show, Eq)
+
+data Settings = Settings
+  { mode                  :: Mode
+  , keepIntermediateFiles :: Bool
+  , inputFile             :: FilePath
+  }
+ deriving (Show, Eq)
+
+parseCLIArgs :: IO Settings
+parseCLIArgs = execParser opts
+
+opts :: ParserInfo Settings
+opts = info (settingsParser <**> helper)
+  (  fullDesc
+  <> progDesc "Compile a C FILE"
+  <> header "Small C Compiler" )
+
+settingsParser :: Parser Settings
+settingsParser = Settings <$> modeParser <*> keepIntermediateFilesParser <*> inputFileParser
+
+modeParser :: Parser Mode
+modeParser = preProcessorMode <|> lexerMode <|> parserMode <|> codeGeneratorMode <|> codeEmitterMode <|> assemblerMode <|> linkerMode <|> pure LinkerMode
+
+keepIntermediateFilesParser :: Parser Bool
+keepIntermediateFilesParser = switch ( long "keep" <> short 'K' <> help "Keep intermediate files" )
+
+inputFileParser :: Parser FilePath
+inputFileParser = argument str (metavar "FILE")
+
+preProcessorMode :: Parser Mode
+preProcessorMode = flag' PreProcessorMode
+  (  long "preprocessor"
+  <> short 'E'
+  <> help "Run up to the pre processor" )
+
+lexerMode :: Parser Mode
+lexerMode = flag' (CompilerMode LexerMode)
+  (  long "lex"
+  <> help "Run up to the lexer" )
+
+parserMode :: Parser Mode
+parserMode = flag' (CompilerMode ParserMode)
+  (  long "parse"
+  <> help "Run up to the parser" )
+
+codeGeneratorMode :: Parser Mode
+codeGeneratorMode = flag' (CompilerMode CodeGeneratorMode)
+  (  long "codegen"
+  <> help "Run up to code generation" )
+
+codeEmitterMode :: Parser Mode
+codeEmitterMode = flag' (CompilerMode CodeEmitterMode)
+  (  long "codeemit"
+  <> short 'S'
+  <> help "Run up to code emission" )
+
+assemblerMode :: Parser Mode
+assemblerMode = flag' AssemblerMode
+  (  long "assembler"
+  <> short 'c'
+  <> help "Run up to the assembler" )
+
+linkerMode :: Parser Mode
+linkerMode = flag' LinkerMode
+  (  long "linker"
+  <> help "Run up to the linker" )
+
