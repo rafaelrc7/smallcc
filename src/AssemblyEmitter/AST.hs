e0b0b1170111cd3e504337fd7ee5fb0c2709ef38
rafaelrc7 <contact@rafaelrc.com>
2024-07-28 01:27:38 -0300

    Improve Assembly generation

--- src/AssemblyEmitter/AST.hs
+++ src/AssemblyEmitter/AST.hs
@@ -13,7 +13,7 @@ class Assembly a where
 
 instance Assembly Program where
   emit :: Program -> Text
-  emit (Program func) = emit func `T.append` "\n.section .note.GNU-stack,\"\",@progbits\n\n"
+  emit (Program func) = emit func `T.append` ".section .note.GNU-stack,\"\",@progbits\n\n"
 
 instance Assembly FunctionDefinition where
   emit :: FunctionDefinition -> Text
@@ -21,12 +21,12 @@ instance Assembly FunctionDefinition where
                         `T.append` name `T.append` ":\n"
                         `T.append` instructions
     where name = funcName func
-          instructions = T.concat $ map emit $ funcInstructions func
+          instructions = flip T.snoc '\n' $ T.concat $ map (flip T.snoc '\n' . T.cons '\t' . emit) $ funcInstructions func
 
 instance Assembly Instruction where
   emit :: Instruction -> Text
-  emit Ret = "ret\n"
-  emit Mov {movSrc=src, movDst=dst} = "movl " `T.append` emit src `T.append` ", " `T.append` emit dst `T.append` "\n"
+  emit Ret = "ret"
+  emit Mov {movSrc=src, movDst=dst} = "movl " `T.append` emit src `T.append` ", " `T.append` emit dst
 
 instance Assembly Operand where
   emit :: Operand -> Text
