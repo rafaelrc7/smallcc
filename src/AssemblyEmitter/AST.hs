204fc619bebd187435f6b4f6d01c57c530a2dea4
rafaelrc7 <contact@rafaelrc.com>
2024-08-01 13:10:58 -0300

    Implement assembly code emission binary ops

--- src/AssemblyEmitter/AST.hs
+++ src/AssemblyEmitter/AST.hs
@@ -32,20 +32,35 @@ instance Assembly Instruction where
              \\npopq %rbp\
              \\nret"
   emit Unary {unaryOperand=operand, unaryOp=op} = T.intercalate " " [emit op, emit operand]
+  emit Binary {binaryOperands=(src, dst), binaryOp=op} = emit op `T.append` " " `T.append` emit src `T.append` ", " `T.append` emit dst
+  emit (Idiv operand) = "idivl " `T.append` emit operand
+  emit Cdq = "cdq"
   emit (AllocateStack offset) = "subq " `T.append` literal offset `T.append` ", %rsp"
 
 instance Assembly Operand where
   emit :: Operand -> Text
-  emit (Reg AX)       = "%eax"
-  emit (Reg R10)      = "%r10d"
+  emit (Reg reg)      = emit reg
   emit (Stack offset) = T.pack (show offset) `T.append` "(%rbp)"
   emit (Imm v)        = literal v
   emit _              = ""
+
+instance Assembly Reg where
+  emit :: Reg -> Text
+  emit AX  = "%eax"
+  emit DX  = "%edx"
+  emit R10 = "%r10d"
+  emit R11 = "%r11d"
 
 instance Assembly UnaryOperator where
   emit :: UnaryOperator -> Text
   emit Neg = "negl"
   emit Not = "notl"
+
+instance Assembly BinaryOperator where
+  emit :: BinaryOperator -> Text
+  emit Add  = "addl"
+  emit Sub  = "subl"
+  emit Mult = "imull"
 
 literal :: Int -> Text
 literal v = T.pack $ '$' : show v
