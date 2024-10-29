c30d94ab5c39aa46dd38f71db363031a3001af73
rafaelrc7 <contact@rafaelrc.com>
2024-10-29 00:28:57 -0300

    Fix error message print

--- src/Error.hs
+++ src/Error.hs
@@ -10,6 +10,6 @@ import           System.IO    (stderr)
 
 class (PrettyPrinter a, Locatable a) => Error a where
   putError :: a -> IO ()
-  putError err = hPutStrLn stderr $ loc <> ": Error: " <> pretty err
-    where loc = maybe "" pretty (locate err)
+  putError err = hPutStrLn stderr $ loc <> "Error: " <> pretty err
+    where loc = maybe "" (\loc' -> pretty loc' <> ":") (locate err)
 
